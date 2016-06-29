db3eea15cfd1d7674708d1b01ae8a4987f7157c7
Robin Müller <robin.mueller@outlook.de>
2016-06-29 11:31:33 +0200

    Updated EchoBot example

--- Telegram.Bot.Examples.Echo/Program.cs
+++ Telegram.Bot.Examples.Echo/Program.cs
@@ -1,62 +1,180 @@
-﻿using System;
+using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+
+using Telegram.Bot.Args;
 using Telegram.Bot.Types;
-using File = System.IO.File;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.InputMessageContents;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace Telegram.Bot.Examples.Echo
 {
     class Program
     {
+        private static readonly TelegramBotClient Bot = new TelegramBotClient("Your API key");
+
         static void Main(string[] args)
         {
-            Run().Wait();
+            Bot.OnCallbackQuery += BotOnCallbackQueryReceived;
+            Bot.OnMessage += BotOnMessageReceived;
+            Bot.OnMessageEdited += BotOnMessageReceived;
+            Bot.OnInlineQuery += BotOnInlineQueryReceived;
+            Bot.OnInlineResultChosen += BotOnChosenInlineResultReceived;
+            Bot.OnReceiveError += BotOnReceiveError;
+
+            var me = Bot.GetMeAsync().Result;
+
+            Console.Title = me.Username;
+
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
         }
 
-        static async Task Run()
+        private static void BotOnReceiveError(object sender, ReceiveErrorEventArgs receiveErrorEventArgs)
         {
-            var Bot = new Api("Your Api Key");
+            Debugger.Break();
+        }
 
-            var me = await Bot.GetMe();
+        private static void BotOnChosenInlineResultReceived(object sender, ChosenInlineResultEventArgs chosenInlineResultEventArgs)
+        {
+            Console.WriteLine($"Received choosen inline result: {chosenInlineResultEventArgs.ChosenInlineResult.ResultId}");
+        }
 
-            Console.WriteLine("Hello my name is {0}", me.Username);
-
-            var offset = 0;
-
-            while (true)
-            {
-                var updates = await Bot.GetUpdates(offset);
-
-                foreach (var update in updates)
+        private static async void BotOnInlineQueryReceived(object sender, InlineQueryEventArgs inlineQueryEventArgs)
+        {
+            InlineQueryResult[] results = {
+                new InlineQueryResultLocation
                 {
-                    if (update.Message.Type == MessageType.TextMessage)
+                    Id = "1",
+                    Latitude = 40.7058316f, // displayed result
+                    Longitude = -74.2581888f,
+                    Title = "New York",
+                    InputMessageContent = new InputLocationMessageContent // message if result is selected
                     {
-                        await Bot.SendChatAction(update.Message.Chat.Id, ChatAction.Typing);
-                        await Task.Delay(2000);
-                        var t = await Bot.SendTextMessage(update.Message.Chat.Id, update.Message.Text);
-                        Console.WriteLine("Echo Message: {0}", update.Message.Text);
+                        Latitude = 40.7058316f,
+                        Longitude = -74.2581888f,
                     }
+                },
 
-                    if (update.Message.Type == MessageType.PhotoMessage)
+                new InlineQueryResultLocation
+                {
+                    Id = "2",
+                    Longitude = 52.507629f, // displayed result
+                    Latitude = 13.1449577f,
+                    Title = "Berlin",
+                    InputMessageContent = new InputLocationMessageContent // message if result is selected
                     {
-                        var file = await Bot.GetFile(update.Message.Photo.LastOrDefault()?.FileId);
-
-                        Console.WriteLine("Received Photo: {0}", file.FilePath);
-
-                        var filename = file.FileId+"."+file.FilePath.Split('.').Last();
-
-                        using (var profileImageStream = File.Open(filename, FileMode.Create))
-                        {
-                            await file.FileStream.CopyToAsync(profileImageStream);
-                        }
+                        Longitude = 52.507629f,
+                        Latitude = 13.1449577f
                     }
-
-                    offset = update.Id + 1;
                 }
+            };
 
-                await Task.Delay(1000);
+            await Bot.AnswerInlineQueryAsync(inlineQueryEventArgs.InlineQuery.Id, results, isPersonal: true, cacheTime: 0);
+        }
+
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+
+            if (message == null || message.Type != MessageType.TextMessage) return;
+
+            if (message.Text.StartsWith("/inline")) // send inline keyboard
+            {
+                await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
+
+                var keyboard = new InlineKeyboardMarkup(new[]
+                {
+                    new[] // first row
+                    {
+                        new InlineKeyboardButton("1.1"),
+                        new InlineKeyboardButton("1.2"),
+                    },
+                    new[] // second row
+                    {
+                        new InlineKeyboardButton("2.1"),
+                        new InlineKeyboardButton("2.2"),
+                    }
+                });
+
+                await Task.Delay(500); // simulate longer running task
+
+                await Bot.SendTextMessageAsync(message.Chat.Id, "Choose",
+                    replyMarkup: keyboard);
             }
+            else if (message.Text.StartsWith("/keyboard")) // send custom keyboard
+            {
+                var keyboard = new ReplyKeyboardMarkup(new[]
+                {
+                    new [] // first row
+                    {
+                        new KeyboardButton("1.1"),
+                        new KeyboardButton("1.2"),  
+                    },
+                    new [] // last row
+                    {
+                        new KeyboardButton("2.1"),
+                        new KeyboardButton("2.2"),  
+                    }
+                });
+
+                await Bot.SendTextMessageAsync(message.Chat.Id, "Choose",
+                    replyMarkup: keyboard);
+            }
+            else if (message.Text.StartsWith("/photo")) // send a photo
+            {
+                await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.UploadPhoto);
+
+                const string file = @"<FilePath>";
+
+                var fileName = file.Split('\\').Last();
+
+                using (var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    var fts = new FileToSend(fileName, fileStream);
+
+                    await Bot.SendPhotoAsync(message.Chat.Id, fts, "Nice Picture");
+                }
+            }
+            else if (message.Text.StartsWith("/request")) // request location or contact
+            {
+                var keyboard = new ReplyKeyboardMarkup(new []
+                {
+                    new KeyboardButton("Location")
+                    {
+                        RequestLocation = true
+                    },
+                    new KeyboardButton("Contact")
+                    {
+                        RequestContact = true
+                    }, 
+                });
+
+                await Bot.SendTextMessageAsync(message.Chat.Id, "Who or Where are you?", replyMarkup: keyboard);
+            }
+            else
+            {
+                var usage = @"Usage:
+/inline   - send inline keyboard
+/keyboard - send custom keyboard
+/photo    - send a photo
+/request  - request location or contact
+";
+
+                await Bot.SendTextMessageAsync(message.Chat.Id, usage,
+                    replyMarkup: new ReplyKeyboardHide());
+            }
+        }
+
+        private static async void BotOnCallbackQueryReceived(object sender, CallbackQueryEventArgs callbackQueryEventArgs)
+        {
+            await Bot.AnswerCallbackQueryAsync(callbackQueryEventArgs.CallbackQuery.Id,
+                $"Received {callbackQueryEventArgs.CallbackQuery.Data}");
         }
     }
 }
