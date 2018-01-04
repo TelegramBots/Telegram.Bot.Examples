a004e4275c50c5a4f352312160e2ae2f9f381afe
Karb0f0s <ctac010@yandex.ru>
2018-01-04 12:30:52 +0300

    Update examples to v14.0.0-beta-342 Telegram.Bot library
    * New ReplyKeyboardMarkup syntax - buttons from string[][]
    * New KeyboardButton syntax WithRequestLocation and WithRequestContact

--- Telegram.Bot.Examples.DotNetCoreWebHook/Program.cs
+++ Telegram.Bot.Examples.DotNetCoreWebHook/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace Telegram.Bot.Examples.DotNetCoreWebHook
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
