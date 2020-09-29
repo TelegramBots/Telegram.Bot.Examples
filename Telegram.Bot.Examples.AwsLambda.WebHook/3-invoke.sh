0b0c92dc3f55e348112c337181bc992b1047d1c2
DmitryBryluk <40860014+DmitryBryluk@users.noreply.github.com>
2020-09-29 09:21:56 +0300

    aws lambda webhook sample (#186)

---
+++ Telegram.Bot.Examples.AwsLambda.WebHook/3-invoke.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eo pipefail
+FUNCTION=$(aws cloudformation describe-stack-resource --stack-name lambda-bot --logical-resource-id function --query 'StackResourceDetail.PhysicalResourceId' --output text)
+
+while true; do
+  aws lambda invoke --function-name $FUNCTION --payload fileb://event.json out.json
+  cat out.json
+  echo ""
+  sleep 2
+done
