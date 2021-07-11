69bed707eb878e47e38c261d7a041df588cacc7b
arvindd <1006084+arvindd@users.noreply.github.com>
2021-07-11 20:49:06 +0530

    Added F# polling example.

---
+++ FSharp/Telegram.Bot.Examples.Polling/Configuration.fs
@@ -0,0 +1,10 @@
+// Configuration for Telegram Bot (Fsharp)
+//
+// Copyright (c) 2021 Arvind Devarajan
+// Licensed to you under the MIT License.
+// See the LICENSE file in the project root for more information.
+
+namespace Telegram.Bot.Extensions.Polling.Fsharp
+
+module TelegramBotCfg =
+    let token = "{BOT_TOKEN}"
