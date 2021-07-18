294a135cdbcec1e174f0d1a89571bff2756a2478
karb0f0s <17474471+karb0f0s@users.noreply.github.com>
2021-07-18 12:29:11 +0300

    F# polling example

--- FSharp.Examples.Polling/Configuration.fs
+++ FSharp.Examples.Polling/Configuration.fs
@@ -4,7 +4,7 @@
 // Licensed to you under the MIT License.
 // See the LICENSE file in the project root for more information.
 
-namespace Telegram.Bot.Extensions.Polling.Fsharp
+namespace FSharp.Examples.Polling
 
 module TelegramBotCfg =
-    let token = "{BOT_TOKEN}"
+    let token = "464670786:AAFhV2UkSau7h4yXm86Y5ekX--_rVJHnLMQ"
