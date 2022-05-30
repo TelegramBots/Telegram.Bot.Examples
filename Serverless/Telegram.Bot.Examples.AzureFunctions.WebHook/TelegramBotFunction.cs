b21d45464464131c3e05bb8fa7b31267b555578a
karb0f0s <17474471+karb0f0s@users.noreply.github.com>
2022-05-30 13:37:32 +0300

    modernize AzureFunctions example

--- Serverless/Telegram.Bot.Examples.AzureFunctions.WebHook/TelegramBotFunction.cs
+++ Serverless/Telegram.Bot.Examples.AzureFunctions.WebHook/TelegramBotFunction.cs
@@ -1,43 +1,40 @@
-using System;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Telegram.Bot.Types;
 
-namespace Telegram.Bot.Examples.AzureFunctions.WebHook
+namespace Telegram.Bot.Examples.AzureFunctions.WebHook;
+
+public class TelegramBotFunction
 {
-    public class TelegramBotFunction
+    private readonly UpdateService _updateService;
+
+    public TelegramBotFunction(UpdateService updateService)
     {
-        private readonly UpdateService _updateService;
+        _updateService = updateService;
+    }
 
-        public TelegramBotFunction(UpdateService updateService)
+    [FunctionName("TelegramBot")]
+    public async Task<IActionResult> Update(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
+        HttpRequest request,
+        ILogger logger)
+    {
+        try
         {
-            _updateService = updateService;
+            var body = await request.ReadAsStringAsync();
+            var update = JsonConvert.DeserializeObject<Update>(body);
+
+            await _updateService.EchoAsync(update);
+        }
+        catch (Exception e)
+        {
+            logger.LogInformation("Exception: " + e.Message);
         }
 
-        [FunctionName("TelegramBot")]
-        public async Task<IActionResult> Update(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
-            HttpRequest request,
-            ILogger logger)
-        {
-            try
-            {
-                var body = await request.ReadAsStringAsync();
-                var update = JsonConvert.DeserializeObject<Update>(body);
-
-                await _updateService.EchoAsync(update);
-            }
-            catch (Exception e)
-            {
-                logger.LogInformation("Exception: " + e.Message);
-            }
-
-            return new OkResult();
-        }
+        return new OkResult();
     }
 }
--- Serverless/Telegram.Bot.Examples.AzureFunctions.WebHook/UpdateService.cs
+++ Serverless/Telegram.Bot.Examples.AzureFunctions.WebHook/UpdateService.cs
@@ -1,34 +1,31 @@
 using Microsoft.Extensions.Logging;
-using System.Threading.Tasks;
 using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
 
-namespace Telegram.Bot.Examples.AzureFunctions.WebHook
+namespace Telegram.Bot.Examples.AzureFunctions.WebHook;
+
+public class UpdateService
 {
-    public class UpdateService
+    private readonly ITelegramBotClient _botClient;
+    private readonly ILogger<UpdateService> _logger;
+
+    public UpdateService(ITelegramBotClient botClient, ILogger<UpdateService> logger)
     {
-        private readonly ITelegramBotClient _botClient;
-        private readonly ILogger<UpdateService> _logger;
+        _botClient = botClient;
+        _logger = logger;
+    }
 
-        public UpdateService(ITelegramBotClient botClient, ILogger<UpdateService> logger)
-        {
-            _botClient = botClient;
-            _logger = logger;
-        }
+    public async Task EchoAsync(Update update)
+    {
+        _logger.LogInformation("Invoke telegram update function");
 
-        public async Task EchoAsync(Update update)
-        {
-            _logger.LogInformation("Invoke telegram update function");
+        if (update is null)
+            return;
 
-            if (update is null)
-                return;
+        if (!(update.Message is { } message)) return;
 
-            if (update.Type == UpdateType.Message)
-            {
-                var message = update.Message;
-                _logger.LogInformation("Received Message from {0}", message.Chat.Id);
-                await _botClient.SendTextMessageAsync(message.Chat, $"Echo : {message.Text}");
-            }
-        }
+        _logger.LogInformation("Received Message from {0}", message.Chat.Id);
+        await _botClient.SendTextMessageAsync(
+            chatId: message.Chat.Id,
+            text: $"Echo : {message.Text}");
     }
 }
