f35aff2dd85fe2ff6781d078dd6bebdd1d48953f
Wizou <11647984+wiz0u@users.noreply.github.com>
2024-08-23 21:51:01 +0200

    MiniApp: Turn Razor website into a Telegram Mini-App

--- MiniApp/Program.cs
+++ MiniApp/Program.cs
@@ -1,4 +1,12 @@
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
 var builder = WebApplication.CreateBuilder(args);
+var token = builder.Configuration["BotToken"]!;             // set your bot token in appsettings.json
+var webappUrl = builder.Configuration["BotWebAppUrl"]!;     // set your bot webapp public url in appsettings.json
+builder.Services.ConfigureTelegramBot<Microsoft.AspNetCore.Http.Json.JsonOptions>(opt => opt.SerializerOptions);
+builder.Services.AddHttpClient("tgwebhook").RemoveAllLoggers().AddTypedClient(httpClient => new TelegramBotClient(token, httpClient));
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -22,4 +30,22 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
+// Bot entrypoints:
+app.MapGet("/bot/setWebhook", async (TelegramBotClient bot) => { await bot.SetWebhookAsync(webappUrl + "/bot"); return $"Webhook set to {webappUrl}/bot"; });
+app.MapPost("/bot", OnUpdate);
+
 app.Run();
+
+
+async void OnUpdate(TelegramBotClient bot, Update update, ILogger<Program> logger)
+{
+    switch (update)
+    {
+        case { Message.Text: "/start" }:
+            await bot.SendTextMessageAsync(update.Message.Chat, "<b>Let's get started</b>",
+                parseMode: Telegram.Bot.Types.Enums.ParseMode.Html,
+                replyMarkup: (InlineKeyboardMarkup)InlineKeyboardButton.WithWebApp("Launch Mini-App", webappUrl));
+            break;
+        default: break;
+    }
+}
---
+++ MiniApp/README.md
@@ -0,0 +1,8 @@
+# Telegram.Bot Mini-App example
+
+This example was built starting from Visual Studio template project: ASP.NET Core Web App (Razor Pages)
+
+Minimal changes were made:
+- Program.cs: for the bot webhook and starting the example website as a Telegram Mini-App
+- _Layout.cshtml: importing required telegram-web-app.js
+
